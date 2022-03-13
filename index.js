db6c4ff845d6d672e944e60d38cfd2ad1d5a6ea0
Mark Pirogov <m4rkpirogov@gmail.com>
2022-03-13 21:38:00 +0300

    fix: script refactoring
    Signed-off-by: Mark Pirogov <m4rkpirogov@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Elements
 const profileName = document.querySelector(".profile__title"),
   profileJob = document.querySelector(".profile__subtitle"),
   editBtn = document.querySelector(".profile__edit-btn"),
@@ -6,37 +5,36 @@ const profileName = document.querySelector(".profile__title"),
   popupForm = document.querySelector(".popup__form"),
   nameInput = document.querySelector(".popup__field_value_name"),
   jobInput = document.querySelector(".popup__field_value_job"),
-  closeBtn = document.querySelector(".popup__close-btn"),
-  saveBtn = document.querySelector(".popup__save-btn");
+  closeBtn = document.querySelector(".popup__close-btn");
 
-// Open & close popup
-const togglePopup = function () {
-  [editBtn, closeBtn].forEach((el) =>
-    el.addEventListener("click", (e) => {
-      e.preventDefault();
-
-      if (e.target === el) {
-        nameInput.value = profileName.textContent;
-        jobInput.value = profileJob.textContent;
-        popup.classList.toggle("popup_opened");
-      }
-    })
-  );
+const openPopup = function () {
+  popup.classList.add("popup_opened");
 };
 
-// Save profile
-const saveProfile = function () {
-  popupForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popup.classList.remove("popup_opened");
-  });
+const closePopup = function () {
+  popup.classList.remove("popup_opened");
 };
 
-// Initialization function
+const saveProfile = function (e) {
+  e.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+};
+
+const editProfile = function () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  openPopup();
+};
+
+const registerPopupEventsListeners = function () {
+  editBtn.addEventListener("click", editProfile);
+  closeBtn.addEventListener("click", closePopup);
+  popupForm.addEventListener("submit", saveProfile);
+};
+
 const init = function () {
-  togglePopup();
-  saveProfile();
+  registerPopupEventsListeners();
 };
 init();
