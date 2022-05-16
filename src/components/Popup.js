23001af6bdde32aa4c4832ab27ec6ed299e48303
Mark Pirogov <m4rkpirogov@gmail.com>
2022-05-17 00:08:17 +0300

    fix: keydown event remove handler & card adding order reverse
    Signed-off-by: Mark Pirogov <m4rkpirogov@gmail.com>

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,18 +2,19 @@ export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._closeButton = this._popup.querySelector(".popup__close-btn");
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add("popup_opened");
 
-    document.addEventListener("keydown", (e) => this._handleEscClose(e));
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove("popup_opened");
 
-    document.removeEventListener("keydown", (e) => this._handleEscClose(e));
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
   _handleEscClose(e) {
--- src/components/Section.js
+++ src/components/Section.js
@@ -12,6 +12,6 @@ export default class Section {
   }
 
   addItem(element) {
-    this._container.append(element);
+    this._container.prepend(element);
   }
 }
